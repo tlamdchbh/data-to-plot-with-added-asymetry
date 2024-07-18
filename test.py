e708eb0b21c47b477338c005357a743c8745e19a
tlamdchbh <Amakarov@thorlabs.com>
2024-07-18 14:06:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,51 @@ import streamlit as st
 from scipy.signal import find_peaks, peak_widths
 
 st.write("hw")
+
+def find_asymmetry(data, peak_prominence, filename, rel_height):
+    fig = px.line(data, x="wave_nm", y="int", title=f'Spectra {filename}')
+    peaks, properties = find_peaks(data["int"], prominence=peak_prominence)
+
+    asymmetry_factor_list = []
+    for i, peak in enumerate(peaks):
+        widths, heights, left_ips, right_ips = peak_widths(data["int"], [peak], rel_height=rel_height)
+
+        # Convert interpolated indices to x-axis values
+        left_x = np.interp(left_ips[0], range(len(data)), data['wave_nm'])
+        right_x = np.interp(right_ips[0], range(len(data)), data['wave_nm'])
+        peak_x = data['wave_nm'].iloc[peak]
+
+        # Calculate asymmetry factor
+        asymmetry_factor = (right_x - peak_x) / (peak_x - left_x)
+
+        asymmetry_factor_list.append(asymmetry_factor)
+        # print(
+        #     f"Peak {i + 1}: Left HW = {left_half_width:.2f}, Right HW = {right_half_width:.2f}, Asymmetry Factor = {asymmetry_factor:.3f}")
+
+        # Add annotation to the plot
+        fig.add_annotation(x=peak_x, y=data['int'].iloc[peak],
+                           text=f'AF: {asymmetry_factor:.3f}',
+                           showarrow=True,
+                           arrowhead=2)
+
+        # Add vertical lines for peak boundaries
+        fig.add_vline(x=left_x, line_dash="dash", line_color="red")
+        fig.add_vline(x=right_x, line_dash="dash", line_color="red")
+
+    # fig.write_html(f'spectra_{peak_prominence}_{filename}.html')
+    return fig, asymmetry_factor_list
+
+st.header('Spectra symmetry factor calculator')
+
+with st.expander("Important", icon="🚨"):
+    st.info("! column name: wave_nm and int")
+    st.info("! separator is whitespace or tab")
+    st.info("! file format txt or csv")
+    st.write("https://www.shimadzu.com/an/service-support/technical-support/analysis-basics/basic/theoretical_plate.html")
+    formula = r'''
+    $$ 
+    Symmetry factor =  \frac{right_x - peak_x}{peak_x - left_x} 
+    $$ 
+    '''
+    st.write(formula)
+    st.image("img_formula.jpg")
